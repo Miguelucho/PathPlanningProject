7fc8637e2afe9efc662765783cd812c2916af50b
Miguelucho <miguelacho.0806@gmail.com>
2018-08-15 09:43:48 -0400

    Fix: - Solved problem to load project      - Solved problem to load the map map-u.yaml      - Solved problem to launch file home_service..sh
      Cambios a ser confirmados:
    	modificado:     catkin_ws/src/official_ros/turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch
    	modificado:     catkin_ws/src/official_ros/turtlebot_interactions/turtlebot_rviz_launchers/rviz/markers.rviz
    	modificado:     catkin_ws/src/official_ros/turtlebot_simulator/turtlebot_gazebo/launch/amcl_mode.launch
    	modificado:     catkin_ws/src/shell_scripts/add_markers.sh
    	modificado:     catkin_ws/src/shell_scripts/home_service.sh
    	modificado:     catkin_ws/src/shell_scripts/launch.sh
    	modificado:     catkin_ws/src/shell_scripts/pick_objects.sh
    	modificado:     catkin_ws/src/shell_scripts/test_navigation.sh
    	modificado:     catkin_ws/src/shell_scripts/test_slam.sh
    	modificado:     catkin_ws/src/shell_scripts/test_slam_turtlebot.sh
    	modificado:     catkin_ws/src/shell_scripts/test_slam_uchobot.sh
    	modificado:     catkin_ws/src/shell_scripts/wall_follower.sh
    	modificado:     catkin_ws/src/shell_scripts/wall_follower_turtlebot.sh
    	modificado:     catkin_ws/src/shell_scripts/wall_follower_uchobot.sh

--- catkin_ws/build/official_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/workspaceROS/RoboND-HomeServiceRobot/catkin_ws/src/official_ros
-# Build directory: /home/nvidia/workspaceROS/RoboND-HomeServiceRobot/catkin_ws/build/official_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/src/shell_scripts/home_service.sh
+++ catkin_ws/src/shell_scripts/home_service.sh
@@ -3,10 +3,10 @@
 export TURTLEBOT_3D_SENSOR=kinect
 xterm -e " roslaunch official_ros turtlebot_world.launch world_file:=/home/nvidia/workspaceROS/RoboND-HomeServiceRobot/catkin_ws/src/world/world_u.world " & 
 sleep 5
-xterm -e " roslaunch official_ros amcl_mode.launch map_file:=/home/nvidia/workspaceROS/RoboND-HomeServiceRobot/catkin_ws/src/world/maps/map_u.yaml 3d_sensor:=kinect" &
+xterm -e " roslaunch official_ros amcl_mode.launch " &
 sleep 5
 xterm -e " roslaunch official_ros view_navigation.launch " &
 sleep 5
-xterm -e " rosrun add_markers add_markers "
+xterm -e " rosrun add_markers add_markers " &
 sleep 5
 xterm -e " rosrun pick_objects pick_objects "
